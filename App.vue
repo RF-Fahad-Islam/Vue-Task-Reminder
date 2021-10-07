388fdbbf4fe1021b7a99e6f2c448fafe0677af35
Fahad <68589534+RF-Fahad-Islam@users.noreply.github.com>
2021-10-07 21:20:18 +0600

    Adding Routers

---
+++ App.vue
@@ -0,0 +1,61 @@
+<template>
+<div id="nav">
+  <router-link :to="{name:'Home'}" >Home</router-link>
+  <router-link :to="{name:'About'}"  >About</router-link>
+</div>
+ <transition
+    name="custom-classes-transition"
+    enter-active-class="animated bounceInUp"
+    leave-active-class="animated fadeOut"
+  >
+  <router-view></router-view>
+</transition>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: cursive, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+  box-sizing: border-box;
+}
+.container {
+  width: 85%;
+  margin: auto;
+}
+
+#nav{
+  display: block;
+  margin: 6px auto;
+}
+
+#nav a {
+  text-decoration: none;
+  font-size: 20px;
+  margin: 5px;
+  transition: all 0.5s;
+}
+
+#nav a:hover {
+  color: blue;
+}
+
+.router-link-active {
+  color: rgb(17, 235, 10);
+  border-bottom: 3px solid green;
+}
+</style>
---
+++ views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="container">
+
+    <h3>
+      A simple task app made by Fahad with the javascript framework called Vue.\
+      <br />
+      This is my first project made with Vue framework! Some unnecessary text
+      here to make this page big LOL! <br />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum temporibus
+      consequuntur alias fugiat tempore rem. Et vero deleniti libero optio
+      dignissimos odio sapiente, aut eum, dolorem laborum ducimus numquam minus
+      fugit mollitia quidem iusto omnis exercitationem nulla hic accusantium!
+      <br>
+      Minima nostrum magnam excepturi natus cumque, iure totam molestias quaerat
+      explicabo sit eaque nobis repellat in ipsa. Dolore facilis inventore,
+      veniam tenetur culpa ipsa illum corporis dolorem quisquam asperiores illo
+      totam molestiae veritatis sed ipsam, voluptatibus in. Error nisi officiis
+      porro molestiae sed aperiam vel, eius ratione nobis quia quas inventore
+      eligendi dolores doloremque eum quae, minus qui soluta! Tenetur,
+      voluptatem.
+    </h3>
+  </div>
+    </div>
+</template>
