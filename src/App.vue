fc676ed01f1e24e30311ac4321c1e902a3c71b40
Fahad <68589534+RF-Fahad-Islam@users.noreply.github.com>
2021-10-07 21:15:02 +0600

    Adding routers and some transitions

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,21 @@
 <template>
-  <Header title="Task Reminder" />
-  <div class="container">
-    <Tasks
-      @toggle-reminder="toggleReminder"
-      @add-task="addTask"
-      @delete-task="deleteTask"
-      :tasks="tasks"
-      :taskId="taskId"
-    />
-  </div>
-  <input type="hidden" :value="setToLocalStorage" />
-  <!-- Set to localstorage  -->
-  <footer style="position: relative; bottom: 0px; text-align: center; display: block; margin: 10px auto;">
-    Copyright&copy; Made By Fahad
-  </footer>
+<div id="nav">
+  <router-link :to="{name:'Home'}" >Home</router-link>
+  <router-link :to="{name:'About'}"  >About</router-link>
+</div>
+ <transition
+    name="custom-classes-transition"
+    enter-active-class="animated bounceInUp"
+    leave-active-class="animated fadeOut"
+  >
+  <router-view></router-view>
+</transition>
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-import Tasks from "./components/Tasks.vue";
-
 export default {
-  name: "App",
-  components: {
-    Header,
-    Tasks,
-  },
-  data() {
-    return {
-      tasks: [],
-      tasksArr: JSON.parse(localStorage.getItem("tasks")),
-      taskId: 1
-    };
-  },
-
-  methods: {
-    addTask(taskObj) {
-      this.tasks = [...this.tasks, taskObj];
-      this.taskId+=1
-      console.log(this.tasks);
-    },
-    deleteTask(id) {
-      this.tasks = this.tasks.filter((task) => task.id !== id);
-      console.log("task", this.tasks);
-    },
-    toggleReminder(id) {
-      this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
-      );
-      console.log("task", this.tasks);
-    },
-  },
-  computed: {
-    setToLocalStorage: function () {
-      localStorage.setItem("tasks", JSON.stringify(this.tasks));
-      return "Setted";
-    },
-  },
-  created() {
-    this.tasks = this.tasksArr || this.tasksArr == [] ? this.tasksArr : [];
-    this.taskId = this.tasks.length>0? (this.tasks[this.tasks.length - 1].id)+1: 1
-  },
-};
+  name: "App"
+}
 </script>
 
 <style>
@@ -76,11 +30,32 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 30px;
+  margin-top: 20px;
   box-sizing: border-box;
 }
 .container {
   width: 85%;
   margin: auto;
+}
+
+#nav{
+  display: block;
+  margin: 6px auto;
+}
+
+#nav a {
+  text-decoration: none;
+  font-size: 20px;
+  margin: 5px;
+  transition: all 0.5s;
+}
+
+#nav a:hover {
+  color: blue;
+}
+
+.router-link-active {
+  color: rgb(17, 235, 10);
+  border-bottom: 3px solid green;
 }
 </style>
